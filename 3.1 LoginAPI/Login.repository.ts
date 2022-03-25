790709fe37dd7e57210366474e85054ab88bd3e9
Dhairya Joshi <66521710+Dhairya-Joshi905@users.noreply.github.com>
2022-03-25 15:11:39 +0530

    Delete 3.1 LoginAPI directory

--- 3.1 LoginAPI/Login.repository.ts
+++
@@ -1,8 +0,0 @@
-import {db} from "../models/index"
-import {User} from "../models/user"
-
-export class LoginRepository{
-    public async getUserByEmail (Email: string): Promise<User|null> {
-        return db.User.findOne({ where: { Email: Email } });
-    }
-}
