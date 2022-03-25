f72cd3f080ef3fcd8198833fa3dbccfcdc14e3ca
Dhairya Joshi <66521710+Dhairya-Joshi905@users.noreply.github.com>
2022-03-25 15:11:03 +0530

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,57 +1,67 @@
+import multer from "multer";
 import express from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
 import {sequelize} from "./models";
-import router from "./routes";
-//import multer from "multer";
-// import swaggerJSDoc from "swagger-jsdoc";
-// import swaggerUI from "swagger-ui-express";
-// import swaggerUi from "swagger-ui-express";
-// const internalDoc = require('./swagger/swagger.json');
+
+import contactUs from "./routes/ContactUs";
+import login from "./routes/LoginSignUp";
+import bookservice from "./routes/bookService";
+import customer from "./routes/customerPages";
+import sp from "./routes/SPPages";
 
 require('dotenv').config();
-const app = express();
 
-// const swaggerOptions={
-//     definition:{
-//         openapi:'3.0.0',
-//         info:{
-//             title:'Contact_Us API',
-//             version: '1.0.0',
-//             description:'',
-//             contact:{
-//                 name:'Dhairya Joshi',
-//                 email:'dhairyajoshi.905@gmail.com'
-//             },
-//             servers:[
-//                 {url: "http://localhost:3000"}
-//             ]
-//         }
-//     },
-//     apis:["routes.ts"]
-// }
-// const swaggerDocs = swaggerJSDoc(swaggerOptions);
-// app.use('/', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+const app = express();
+const swaggerOption={
+    definition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Helperland API',
+            version: '1.0.0',
+            description:'Helperland',
+            contact:{
+                name:'Dhairya Joshi',
+                email:'dhairyajoshi.905@gmail.com'
+            },
+            servers:[{url: "http://localhost:3000"}]
+        }
+    },
+    apis: [
+        './routes/ContactUs.ts',
+        './routes/LoginSignUp.ts',
+        './routes/bookService.ts',
+        './routes/customerPages.ts',
+        './routes/SPPages.ts'
+    ]
+}
+const swaggerDocs = swaggerJSDoc(swaggerOption);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-//app.use(multer({dest:'uploadFiles'}).single('file'));
+app.use(express.urlencoded({ extended: false }));
+app.use(multer({ dest: 'UploadedFiles' }).single('file'));
 
-app.use('/', router);
-
-//app.use('/swagger', swaggerUi.serve, swaggerUi.setup(internalDoc));
+app.use('/Helperland/ContactUs', contactUs);
+app.use('/Helperland/SignUp&Login', login);
+app.use('/Helperland/BookService', bookservice);
+app.use('/Helperland/CustomerPages', customer);
+app.use('/Helperland/SPPages', sp);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server rocking at ${process.env.PORT}`)
-    sequelize.authenticate().then(async() => {
-        console.log("database connected");
+    sequelize.authenticate()
+    .then(async() => {
+        console.log('database connected');
         try {
-            await sequelize.sync();
-        } catch (error) {
-            console.log(error);
+            await sequelize.sync(/*{force: true}*/);
+        }
+        catch (err) {
+            console.log(err);
         }
     })
-    .catch( (e: any) => {
+    .catch((e: Error) => {
         console.log(e.message)
     })
 })
-
-// export { app };
